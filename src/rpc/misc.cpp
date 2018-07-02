b9a0131eb901d6aabf136a435273c292aafe432f
l3onL <leon.yliu@gmail.com>
2018-07-03 01:28:24 +0800

    Mac and Windows build

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -617,7 +617,7 @@ UniValue echo(const JSONRPCRequest& request)
             "echo|echojson \"message\" ...\n"
             "\nSimply echo back the input arguments. This command is for testing.\n"
             "\nThe difference between echo and echojson is that echojson has argument conversion enabled in the client-side table in"
-            "bitcoin-cli and the GUI. There is no server-side difference."
+            "acs-cli and the GUI. There is no server-side difference."
         );
 
     return request.params;

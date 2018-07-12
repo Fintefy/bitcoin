d92e28abdfbf631b2d57815cfe239938e2616df0
l3onL <leon.yliu@gmail.com>
2018-07-12 21:54:01 +0800

    magic number change

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -116,7 +116,7 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfa;
+        pchMessageStart[0] = 0xfb;
         pchMessageStart[1] = 0xbb;
         pchMessageStart[2] = 0xbc;
         pchMessageStart[3] = 0xdd;
